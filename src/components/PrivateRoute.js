16cb7adfbab0f755da1f4157703a2ed61d55977b
Sebastian <sebas6292@gmail.com>
2021-09-18 10:25:38 -0700

    added privateRoute and Bubble Page

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,2 +1,20 @@
 //Task List:
 //1. Build a PrivateRoute component that redirects if user is not logged in
+
+import React from 'react'; 
+import { Route, Redirect } from 'react-router-dom';
+
+function PrivateRoute({ components: Component, ...rest }) {
+    return (
+        <Route 
+            {...rest} render={() => {
+                if(window.localStorage.getItem('token')) {
+                    return <Component />;
+                } else {
+                    return <Redirect to='/login'/>;
+                }
+            }}
+            />
+    )
+}
+export default PrivateRoute;
